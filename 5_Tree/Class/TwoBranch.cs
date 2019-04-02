3b7f176e415a64fbf9eeea77e028b037bfc21901
lurenjia <ljiguang@163.com>
2019-04-03 07:08:08 +0800

    添加项目文件。

---
+++ 5_Tree/Class/TwoBranch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Tree.Class
+{
+    /// <summary>
+    /// 通过二叉链表存储二叉树时的节点
+    /// </summary>
+    public class TwoBranchNode<T>
+    {
+        /// <summary>
+        /// 数据域
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// 左孩子节点引用域
+        /// </summary>
+        public TwoBranchNode<T> LeftChild { get; set; }
+
+        /// <summary>
+        /// 右孩子节点引用域
+        /// </summary>
+        public TwoBranchNode<T> RightChild { get; set; }
+
+        public TwoBranchNode()
+        {
+
+        }
+    }
+}
