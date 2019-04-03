34d98d98a13f2dfe2c5ef52154e75b18f54e5c2b
liujgwork <liujg_dr@163.com>
2019-04-03 11:39:31 +0800

    1、修改类名 2、增加二叉树的三叉链表存储方式

---
+++ 5_Tree/Class/BinaryTreeThreeBranch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Tree.Class
+{
+    /// <summary>
+    /// 通过三叉链表存储二叉树时的节点
+    /// </summary>
+    public class ThreeBranchNode<T>
+    {
+        private ThreeBranchNode<T> parent;
+
+        private ThreeBranchNode<T> leftChild;
+
+        private ThreeBranchNode<T> rightChild;
+
+        private T value;
+
+        /// <summary>
+        /// 节点的双亲节点
+        /// </summary>
+        public ThreeBranchNode<T> Parent { get => parent; set => parent = value; }
+
+        /// <summary>
+        /// 节点的左孩子节点
+        /// </summary>
+        public ThreeBranchNode<T> LeftChild { get => leftChild; set => leftChild = value; }
+
+        /// <summary>
+        /// 节点的右孩子节点
+        /// </summary>
+        public ThreeBranchNode<T> RightChild { get => rightChild; set => rightChild = value; }
+
+        /// <summary>
+        /// 节点的值
+        /// </summary>
+        public T Value { get => value; set => this.value = value; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ThreeBranchNode()
+        {
+
+        }
+    }
+}
